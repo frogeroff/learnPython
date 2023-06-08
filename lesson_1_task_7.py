a987a353a6ba74da9231a116997b7e29d5e0d484
frogeroff <frogeroff1@gmail.com>
2023-06-08 14:17:14 +0500

    Commit08.06

--- lesson_1_task_7.py
+++ lesson_1_task_7.py
@@ -1,13 +1,33 @@
-def happy_numbers(a, b, c, d, e, f, g, h, i, j):
+def happy_numbers(a):
     print(a)
+happy_numbers(1)
+def happy_numbers(b):
     print(b)
+happy_numbers(2)
+def happy_numbers(c):
     print(c)
+happy_numbers(3)
+def happy_numbers(d):
     print(d)
+happy_numbers(4)
+def happy_numbers(e):
     print(e)
+happy_numbers(5)
+def happy_numbers(f):
     print(f)
+happy_numbers(6)
+def happy_numbers(g):
     print(g)
+happy_numbers(7)
+def happy_numbers(h):
     print(h)
+happy_numbers(8)
+def happy_numbers(i):
     print(i)
+happy_numbers(9)
+def happy_numbers(j):
     print(j)
-    print(h, h, j, j, e, e, e, c, e, c, e)
-happy_numbers(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+happy_numbers(0)
+def happy_numbers(a, b, c, d):
+    print(d, d, a, a, c, c, c, b, c, b, c)
+happy_numbers(0, 3, 5, 8)
