8d0317b19342a64096642e83288581eb1bcea144
frogeroff <frogeroff1@gmail.com>
2023-06-16 13:30:36 +0500

    Commit1606

--- lesson_1_task_7.py
+++ lesson_1_task_7.py
@@ -1,33 +1,41 @@
-def happy_numbers(a):
-    print(a)
-happy_numbers(1)
-def happy_numbers(b):
-    print(b)
-happy_numbers(2)
-def happy_numbers(c):
-    print(c)
-happy_numbers(3)
-def happy_numbers(d):
-    print(d)
-happy_numbers(4)
-def happy_numbers(e):
-    print(e)
-happy_numbers(5)
-def happy_numbers(f):
-    print(f)
-happy_numbers(6)
-def happy_numbers(g):
-    print(g)
-happy_numbers(7)
-def happy_numbers(h):
-    print(h)
-happy_numbers(8)
-def happy_numbers(i):
-    print(i)
-happy_numbers(9)
-def happy_numbers(j):
-    print(j)
-happy_numbers(0)
-def happy_numbers(a, b, c, d):
-    print(d, d, a, a, c, c, c, b, c, b, c)
-happy_numbers(0, 3, 5, 8)
+def happy_numbers1():
+    print(1)
+happy_numbers1()
+def happy_numbers2():
+    print(2)
+happy_numbers2()
+def happy_numbers3():
+    print(3)
+happy_numbers3()
+def happy_numbers4():
+    print(4)
+happy_numbers4()
+def happy_numbers5():
+    print(5)
+happy_numbers5()
+def happy_numbers6():
+    print(6)
+happy_numbers6()
+def happy_numbers7():
+    print(7)
+happy_numbers7()
+def happy_numbers8():
+    print(8)
+happy_numbers8()
+def happy_numbers9():
+    print(9)
+happy_numbers9()
+def happy_numbers0():
+    print(0)
+happy_numbers0()
+happy_numbers8()
+happy_numbers8()
+happy_numbers0()
+happy_numbers0()
+happy_numbers5()
+happy_numbers5()
+happy_numbers5()
+happy_numbers3()
+happy_numbers5()
+happy_numbers3()
+happy_numbers5()
