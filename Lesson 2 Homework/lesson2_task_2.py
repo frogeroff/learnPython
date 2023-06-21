cf5b39fd7f44cf91405170d4d91d873ef365d793
frogeroff <frogeroff1@gmail.com>
2023-06-21 17:41:14 +0500

    2106

--- Lesson 2 Homework/lesson2_task_2.py
+++ Lesson 2 Homework/lesson2_task_2.py
@@ -1,11 +1,8 @@
-
-n = input("Введите год: ")
-if(int(n) % 4 == 0):
-    print(True)
-else:
-    print(False)
-
-def is_year_leap(year):
-    print("год", year,":")
-is_year_leap(n)
+def is_year_leap():
+    n = input("Введите год: ")
+    if(int(n) % 4 == 0):
+        print("год", n+":", True)
+    else:
+        print("год", n+":", False)
+is_year_leap()
 
